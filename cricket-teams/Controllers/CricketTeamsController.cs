bed0cee7e863180fdd3629c7bce38b6fe822eac6
Abhishek Patil <abhishek.hpatil63@gmail.com>
2024-04-15 22:39:42 +0530

    All endpoints added for CRUD

--- cricket-teams/Controllers/CricketTeamsController.cs
+++ cricket-teams/Controllers/CricketTeamsController.cs
@@ -9,13 +9,62 @@ namespace cricket_teams.Controllers
     {
         private readonly CricketTeamRepository _repository = repository;
 
-        [HttpGet]
+        [HttpGet("GetTeams")]
         public async Task<ActionResult<List<CricketTeam>>> GetTeams()
         {
             var teams = await _repository.GetAllTeamsAsync();
             return Ok(teams);
         }
 
-        // Implement other CRUD actions as needed
+        [HttpGet("GetTeamById/{id}")]
+        public async Task<ActionResult<CricketTeam>> GetTeamById(int id)
+        {
+            var team = await _repository.GetTeamByIdAsync(id);
+            if (team == null)
+            {
+                return NotFound();
+            }
+            return team;
+        }
+
+        [HttpPost("CreateTeam")]
+        public async Task<ActionResult<CricketTeam>> CreateTeam(CricketTeam team)
+        {
+            if (team == null)
+            {
+                return BadRequest();
+            }
+
+            await _repository.AddTeamAsync(team);
+
+            return CreatedAtAction(nameof(GetTeamById), new { id = team.TeamId }, team);
+        }
+
+        [HttpPut("UpdateTeam/{id}")]
+        public async Task<IActionResult> UpdateTeam(int id, CricketTeam team)
+        {
+            if (id != team.TeamId)
+            {
+                return BadRequest();
+            }
+
+            await _repository.UpdateTeamAsync(team);
+
+            return NoContent();
+        }
+
+        [HttpDelete("DeleteTeam/{id}")]
+        public async Task<IActionResult> DeleteTeam(int id)
+        {
+            var team = await _repository.GetTeamByIdAsync(id);
+            if (team == null)
+            {
+                return NotFound();
+            }
+
+            await _repository.DeleteTeamAsync(id);
+
+            return NoContent();
+        }
     }
 }
--- cricket-teams/Repository/CricketTeamRepository.cs
+++ cricket-teams/Repository/CricketTeamRepository.cs
@@ -3,18 +3,40 @@ using Microsoft.EntityFrameworkCore;
 
 namespace cricket_teams.Repository
 {
-    public class CricketTeamRepository
+    public class CricketTeamRepository(ApplicationDbContext context)
     {
-        private readonly ApplicationDbContext _context;
-
-        public CricketTeamRepository(ApplicationDbContext context)
-        {
-            _context = context;
-        }
+        private readonly ApplicationDbContext _context = context;
 
         public async Task<List<CricketTeam>> GetAllTeamsAsync()
         {
             return await _context.CricketTeams.ToListAsync();
+        }
+
+        public async Task<CricketTeam?> GetTeamByIdAsync(int id)
+        {
+            return await _context.CricketTeams.FindAsync(id);
+        }
+
+        public async Task AddTeamAsync(CricketTeam team)
+        {
+            _context.CricketTeams.Add(team);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateTeamAsync(CricketTeam team)
+        {
+            _context.Entry(team).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteTeamAsync(int id)
+        {
+            var team = await GetTeamByIdAsync(id);
+            if (team != null)
+            {
+                _context.CricketTeams.Remove(team);
+                await _context.SaveChangesAsync();
+            }
         }
     }
 }
